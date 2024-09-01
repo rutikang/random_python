f8cb9e254353ff9d5a4e1d540cb02183f46ffc95
Daniel Kasigazi Rutikanga <117582332+rutikang@users.noreply.github.com>
2024-09-01 22:17:24 +0200

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,16 @@
+# print("It works")
+# print(343443)
+
+mylist = ["daniel" , "janet", "kasigazi"]
+
+newlist =[]
+
+# To do
+"""
+- Add each item to the new list one at a time
+- make it capital before adding
+"""
+for x in mylist:
+    newlist.append(x.upper())
+
+print(newlist)
